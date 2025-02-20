09dd74cd5d0e97a3ceb84ee9a6cee6bbe9dacf3e
nahom <nahom.d54@gmail.com>
2025-02-20 19:56:05 +0300

    Rename project from Flask Scaffold to Flask Ignite and update related files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Flask Scaffold
+# Flask Iginite
 
 A simple Flask app creator that automatically generates a basic Flask project structure including models, routes, services, configuration, and more.
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="flask_scaffold",
+    name="flask_ignite",
     version="0.1.0",
     description="A simple Flask app creator",
     long_description=open("README.md").read(),
